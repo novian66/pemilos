72fd2e4bdf82d9adaf23efb6b6585d52bed2d6dd
Fauzan Taqiyuddin <19102103@ittelkom-pwt.ac.id>
2022-08-13 17:25:39 +0700

    beta 5

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-
-
 // kpu only route
 Route::prefix('school')->middleware('auth')->group(function () {
     // school 
