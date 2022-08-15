3edbdfca5980cc184043ed7fd4d5d0c2484761a3
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-08-15 14:05:58 +0700

    Beta 18

--- app/Models/Admin/UserJoinElection.php
+++ app/Models/Admin/UserJoinElection.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserJoinElection extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'election_school_id',
+        'school_id'
+    ];
 }
