42454373f892157defc25645ccb7a55183ab6489
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-08-17 18:44:45 +0700

    Beta 19

--- resources/views/admin/school/view.blade.php
+++ resources/views/admin/school/view.blade.php
@@ -71,19 +71,8 @@
                                             <div class="text-muted">{{ $data->nama }}</div>
                                         </div>
                                         <div class="ms-auto">
-                                            <a href="#" class="text-muted">
-                                                <!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                    height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                    stroke="currentColor" fill="none" stroke-linecap="round"
-                                                    stroke-linejoin="round">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                    <circle cx="12" cy="12" r="2"></circle>
-                                                    <path
-                                                        d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
-                                                    </path>
-                                                </svg>
-                                                467
+                                            <a href="{{ route('quickcount') }}" class="text-muted">
+                                                Token: {{ $elec->token }}
                                             </a>
                                         </div>
                                     </div>
@@ -91,10 +80,9 @@
                                 <div class="d-flex mt-1 mb-0">
                                     <a href="{{ route('election.view', ['id' => $data->id, 'election_id' => $elec->id]) }}"
                                         class="card-btn">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted"
-                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                             <circle cx="12" cy="12" r="2"></circle>
                                             <path
